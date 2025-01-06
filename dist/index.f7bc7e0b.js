11223650e8a543e0e396d5d90bd4e7bfad6f8a5e
shachar700 <shachar700@gmail.com>
2025-01-06 12:26:30 +0200

    removed unnecessery files

--- dist/index.f7bc7e0b.js
+++
@@ -1,2 +0,0 @@
-console.log("Checking if already on the login page..."),"/login/login.html"!==window.location.pathname&&(console.log("Going to login"),window.location.href="./login/login.html");
-//# sourceMappingURL=index.f7bc7e0b.js.map
--- index.js
+++
@@ -1,5 +0,0 @@
-console.log("Checking if already on the login page...");
-if (window.location.pathname !== "/login/login.html") {
-    console.log("Going to login");
-    window.location.href = "./login/login.html";
-}
