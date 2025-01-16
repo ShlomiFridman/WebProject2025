263ed71ecdc9e9f9c004a6f1162ad807599eecd3
shachar700 <shachar700@gmail.com>
2025-01-16 23:10:28 +0200

    padding to headers in event headers table

--- touringo/src/components/EventTable.tsx
+++ touringo/src/components/EventTable.tsx
@@ -11,8 +11,8 @@ const EventTable: React.FC<EventTableProps> = ({ events }) => {
     <div className="event-table">
       <div className="event-row flex items-center justify-between">
         <b></b>
-        <h1 className="hidden sm:block"><b>Details</b></h1> {/* Hidden on small screens */}
-        <h1 className="hidden sm:block"><b>Options</b></h1> {/* Hidden on small screens */}
+        <h1 className="hidden sm:block pr-4"><b>Details</b></h1> {/* Hidden on small screens */}
+        <h1 className="hidden sm:block pr-8"><b>Options</b></h1> {/* Hidden on small screens */}
       </div>
       {events.map((event) => (
         <EventRow key={event.event_id} event={event} />
