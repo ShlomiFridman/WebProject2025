042fb7716c7131cfd63f9ad3f721016fbada33e8
ShlomiFridman <shlomif99@gmail.com>
2025-01-17 09:50:44 +0200

    clickable event row

--- touringo/src/components/EventRow.tsx
+++ touringo/src/components/EventRow.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { TR_Event } from "@/utils/classes";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useAppContext } from "@/context/MainContext";
 import BookingButton from "./BookButton";
 
@@ -11,13 +11,21 @@ type EventRowProps = {
 };
 
 const EventRow: React.FC<EventRowProps> = ({ event }) => {
+  const path = usePathname();
   const router = useRouter();
   const { dispatch } = useAppContext();
 
-  const selectEvent = ({ date, tickets }: { date: string; tickets: number }) => {
-    dispatch({ type: "SET_SELECTED_EVENT", payload: event });
+  const createBooking = ({ date, tickets }: { date: string; tickets: number }) => {
     router.push(`/event/${event.event_id}?date=${date}&tickets=${tickets}`);
   };
+
+  const selectEvent = () =>{
+    if (path == `/event/${event.event_id}`)
+      return;
+    dispatch({ type: "SET_SELECTED_EVENT", payload: event });
+    router.push(`/event/${event.event_id}`);
+
+  } 
 
   const openMap = () => {
     const mapUrl = `https://www.google.com/maps?q=${encodeURIComponent(`${event.town}, ${event.address}`)}&z=15&output=embed`;
@@ -26,7 +34,7 @@ const EventRow: React.FC<EventRowProps> = ({ event }) => {
 
   return (
     <div className="event-row flex flex-col sm:flex-row items-center justify-between p-4 mb-4 transition bg-[#e7ccb3] dark:bg-[var(--box-background)]  sm:bg-white hover:bg-[#e7ccb3] hover:rounded-lg hover:shadow-md dark:bg[var(--background)] dark:hover:bg-[var(--box-background)] sm:dark:bg-transparent sm:dark:hover:bg-[var(--box-background)] dark:hover:shadow-lg">
-      <div className="flex flex-col sm:flex-row sm:items-center w-full">
+      <div className="flex flex-col sm:flex-row sm:items-center w-full" onClick={()=>selectEvent()}>
         <div className="max-h-[1000px] mb-4 sm:mb-0 sm:mr-4 sm:w-1/5">
           <Image
             priority
@@ -53,7 +61,7 @@ const EventRow: React.FC<EventRowProps> = ({ event }) => {
         </div>
       </div>
       <div className="mt-4 sm:mt-0 sm:ml-4">
-        <BookingButton onBook={selectEvent} />
+        <BookingButton onBook={createBooking} />
         <br />
         <br />
         <button
