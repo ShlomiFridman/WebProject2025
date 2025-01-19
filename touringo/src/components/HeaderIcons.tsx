e2a0f380f21b92d206a4dd43e25f1e57b637b43c
shachar700 <shachar700@gmail.com>
2025-01-19 11:25:33 +0200

    replaced alert icon with bell

--- touringo/src/components/HeaderIcons.tsx
+++ touringo/src/components/HeaderIcons.tsx
@@ -7,7 +7,7 @@ type HeaderIconsParams = {
   loginFlag: boolean;
 };
 
-const HeaderIcons: React.FC<HeaderIconsParams> = ({loginFlag}) => {
+const HeaderIcons: React.FC<HeaderIconsParams> = ({ loginFlag }) => {
   const { theme, toggleTheme } = useTheme(); // Use theme context
   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Dropdown toggle state
 
@@ -22,7 +22,7 @@ const HeaderIcons: React.FC<HeaderIconsParams> = ({loginFlag}) => {
     <div className="flex justify-end gap-4">
       {/* Alert Icon */}
       {/* hidden until the feature is added */}
-      <div className={`relative hidden ${!loginFlag? 'hidden':''}`}>
+      <div className={`relative hidden ${!loginFlag ? 'hidden' : ''}`}>
         <button
           onClick={toggleDropdown}
           className="p-2 rounded-full cursor-pointer focus:outline-none"
@@ -34,9 +34,10 @@ const HeaderIcons: React.FC<HeaderIconsParams> = ({loginFlag}) => {
             fill="none"
           >
             <path
-              className="fill-gray-800 dark:fill-gray-100" // Dynamic class for light/dark mode
-              d="M12 0C5.37 0 0 5.37 0 12s5.37 12 12 12 12-5.37 12-12S18.63 0 12 0zm0 20.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM13 14h-2V6h2v8z"
-            ></path>
+            strokeWidth={1}
+              className="fill-gray-800 dark:fill-gray-100"
+              d="M11.5 1C10.9477 1 10.5 1.44772 10.5 2V3H9.99998C7.23864 3 4.99999 5.23825 4.99999 7.99975V11C4.99999 11.7377 4.76718 12.5722 4.39739 13.4148C4.03165 14.2482 3.55876 15.0294 3.14142 15.6439C2.38188 16.7624 2.85216 18.5301 4.40564 18.8103C5.42144 18.9935 6.85701 19.2115 8.54656 19.3527C8.54454 19.4015 8.54352 19.4506 8.54352 19.5C8.54352 21.433 10.1105 23 12.0435 23C13.9765 23 15.5435 21.433 15.5435 19.5C15.5435 19.4482 15.5424 19.3966 15.5402 19.3453C17.1921 19.204 18.596 18.9903 19.5943 18.8103C21.1478 18.5301 21.6181 16.7624 20.8586 15.6439C20.4412 15.0294 19.9683 14.2482 19.6026 13.4148C19.2328 12.5722 19 11.7377 19 11V7.99975C19 5.23825 16.7613 3 14 3H13.5V2C13.5 1.44772 13.0523 1 12.5 1H11.5ZM12 19.5C12.5113 19.5 13.0122 19.4898 13.4997 19.4715C13.5076 20.2758 12.8541 20.9565 12.0435 20.9565C11.2347 20.9565 10.5803 20.2778 10.5872 19.4747C11.0473 19.491 11.5191 19.5 12 19.5ZM9.99998 5C8.34305 5 6.99999 6.34298 6.99999 7.99975V11C6.99999 12.1234 6.65547 13.2463 6.22878 14.2186C5.79804 15.2 5.25528 16.0911 4.79599 16.7675C4.78578 16.7825 4.78102 16.7969 4.77941 16.8113C4.77797 16.8242 4.77919 16.8362 4.78167 16.8458C6.3644 17.1303 9.00044 17.5 12 17.5C14.9995 17.5 17.6356 17.1303 19.2183 16.8458C19.2208 16.8362 19.222 16.8242 19.2206 16.8113C19.2189 16.7969 19.2142 16.7825 19.204 16.7675C18.7447 16.0911 18.2019 15.2 17.7712 14.2186C17.3445 13.2463 17 12.1234 17 11V7.99975C17 6.34298 15.6569 5 14 5H9.99998Z"
+              />
           </svg>
         </button>
 
@@ -124,8 +125,8 @@ const HeaderIcons: React.FC<HeaderIconsParams> = ({loginFlag}) => {
         </svg>
       </a>
       <Link
-      href="/login"
-      className={`inline-flex items-center py-2 px-3 text-base font-medium text-gray-900 dark:text-gray-100 rounded-md ${!loginFlag? 'hidden':''}`}>
+        href="/login"
+        className={`inline-flex items-center py-2 px-3 text-base font-medium text-gray-900 dark:text-gray-100 rounded-md ${!loginFlag ? 'hidden' : ''}`}>
         <span className="sr-only">Login</span>
         <svg
           viewBox="0 0 16 16"
@@ -135,7 +136,7 @@ const HeaderIcons: React.FC<HeaderIconsParams> = ({loginFlag}) => {
         >
           <path d="M12.207 9H5V7h7.136L11.05 5.914 12.464 4.5 16 8.036l-3.536 3.535-1.414-1.414L12.207 9zM10 4H8V2H2v12h6v-2h2v4H0V0h10v4z"></path>
         </svg>
-  </Link>
+      </Link>
     </div>
   );
 };
