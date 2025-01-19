e94c3b48d80fbce1152ddaeb3e86f639aa794161
shachar700 <shachar700@gmail.com>
2025-01-19 14:40:55 +0200

    removed not used path const

--- touringo/src/components/EventTable.tsx
+++ touringo/src/components/EventTable.tsx
@@ -8,12 +8,11 @@ type EventTableProps = {
 };
 
 const EventTable: React.FC<EventTableProps> = ({ events }) => {
-  const path = usePathname()
   return (
     <div className="event-table">
       <div className="event-row flex items-center justify-between">
         <b></b>
-        <h1 className="text-center hidden sm:block pr-4"><b>Details</b></h1> {/* Hidden on small screens */}
+        <h1 className="hidden sm:block pr-4"><b>Details</b></h1> {/* Hidden on small screens */}
         <h1 className="hidden sm:block pr-8"><b>Options</b></h1>{/* Hidden on small screens */}
       </div>
       {events.map((event) => (
