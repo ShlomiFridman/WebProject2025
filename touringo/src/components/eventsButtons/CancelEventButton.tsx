75702b1966893f292068a1d138139002f0e7e656
Omer <goldstein.omer96@gmail.com>
2025-01-20 16:52:03 +0200

    fixed 'my events' buttons size

--- touringo/src/components/eventsButtons/CancelEventButton.tsx
+++ touringo/src/components/eventsButtons/CancelEventButton.tsx
@@ -58,7 +58,7 @@ function CancelEventButton({ event }: ButtonProps) {
         <button
             ref={btnRef}
             onClick={cancelRequest}
-            className="bg-red-500 px-4 py-2 m-2 rounded hover:bg-red-700 transition w-full h-full dark:bg-red-700 dark:hover:bg-red-500"
+            className="bg-red-500 px-4 py-2 m-2 rounded hover:bg-red-700 transition w-full  dark:bg-red-700 dark:hover:bg-red-500"
         >
         </button>
     );
