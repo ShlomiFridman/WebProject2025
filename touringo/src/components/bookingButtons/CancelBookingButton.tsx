d96c08b95d974dafa36b2730402c6e0d877b9cd0
shachar700 <shachar700@gmail.com>
2025-01-20 17:51:19 +0200

    cancel in red at booking page

--- touringo/src/components/bookingButtons/CancelBookingButton.tsx
+++ touringo/src/components/bookingButtons/CancelBookingButton.tsx
@@ -57,8 +57,12 @@ function CancelBookingButton({ booking }: ButtonProps) {
   return (
     <button
       ref={btnRef}
-      className="px-4 py-2 rounded"
       onClick={cancelRequest}
+      className={`px-4 m-2 rounded transition w-full h-full ${
+        !booking.hasPassed()
+          ? "bg-red-500 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-500"
+          : ""
+      }`}
     >
     </button>
   );
