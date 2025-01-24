59e088f0f7ad0c103a40f37b172e5b7b10c51879
shachar700 <shachar700@gmail.com>
2025-01-24 11:30:05 +0200

    reverted svg files

---
+++ touringo/src/utils/styles.ts
@@ -0,0 +1,8 @@
+export const myStyles = {
+    button_green: null,
+    button_blue: "inline-flex items-center justify-center bg-blue-500 px-4 py-2 m-2 rounded w-full hover:bg-blue-700 transition whitespace-nowrap dark:bg-blue-700 dark:hover:bg-blue-500 disabled:bg-gray-400",
+    button_red: null,
+
+    eventRow: "event-row flex sm:flex-row items-center justify-between p-4 mb-4 transition bg-[#c6e7b3] dark:bg-[var(--box-background)] sm:bg-white hover:bg-[#c6e7b3] hover:rounded-lg hover:shadow-md dark:bg[var(--background)] dark:hover:bg-[var(--box-background)] sm:dark:bg-[#292b2f] sm:dark:hover:bg-[var(--box-background)] dark:hover:shadow-lg",
+    eventRowSelected: "p-2 outline-dashed rounded outline-1 flex flex-col sm:flex-row sm:items-center w-full"
+}
