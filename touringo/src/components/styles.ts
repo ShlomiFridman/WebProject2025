45c37e93b33defd389746bb9fae425050834931d
shachar700 <shachar700@gmail.com>
2025-01-25 23:11:18 +0200

    changed colors to headers and buttons

--- touringo/src/components/styles.ts
+++ touringo/src/components/styles.ts
@@ -1,14 +1,14 @@
 export const myStyles = {
     //Repeats
-    button_blue: "bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-500",
-    button_green: "bg-green-500 hover:bg-green-700 dark:bg-green-700 dark:hover:bg-green-500",
-    button_red: "bg-red-500 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-500",
+    button_blue: "bg-blue-300 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-500",
+    button_green: "bg-green-700 text-white hover:bg-green-700 dark:bg-green-700 dark:hover:bg-green-500",
+    button_red: "bg-red-600 text-white hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-500",
     container_max_width: "max-w-[1000px] my-4 mx-auto",
-    page_title: "text-3xl text-green-800 dark:text-green-200 font-bold",
+    page_title: "text-3xl text-blue-800 dark:text-blue-200 font-bold",
 
     //Long
     green_container: "w-full h-16 p-4 flex flex-row sm:flex-row justify-between gap-2 bg-green-300 dark:bg-green-900",
-    eventRow: "event-row flex sm:flex-row items-center justify-between p-4 mb-4 transition bg-[#c6e7b3] dark:bg-[var(--box-background)] sm:bg-white hover:bg-[#c6e7b3] hover:rounded-lg hover:shadow-md dark:bg[var(--background)] dark:hover:bg-[var(--box-background)] sm:dark:bg-[#292b2f] sm:dark:hover:bg-[var(--box-background)] dark:hover:shadow-lg",
+    eventRow: "event-row flex sm:flex-row items-center justify-between p-4 mb-4 transition bg-green-100 dark:bg-[var(--box-background)] sm:bg-white hover:bg-green-100 hover:rounded-lg hover:shadow-md dark:bg[var(--background)] dark:hover:bg-[var(--box-background)] sm:dark:bg-[#292b2f] sm:dark:hover:bg-[var(--box-background)] dark:hover:shadow-lg",
 
     eventRowSelected: "p-2 outline-dashed outline-1 rounded flex flex-col sm:flex-row sm:items-center w-full"
 
