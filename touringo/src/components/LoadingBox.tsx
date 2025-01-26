a0aed6c0c04a19cb6678e90731a95249fdb23073
shachar700 <shachar700@gmail.com>
2025-01-26 19:40:57 +0200

    commented some files

--- touringo/src/components/LoadingBox.tsx
+++ touringo/src/components/LoadingBox.tsx
@@ -1,11 +1,15 @@
-"use client";
+"use client"; // Marks this component as a client-side component
 
-
+/**
+ * A functional component that displays a simple loading indicator with a spinner and a message.
+ */
 export default function LoadingBox() {
     return (
         <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg flex items-center space-x-4">
-                <div className="animate-spin h-8 w-8 border-4 border-t-transparent border-blue-500 rounded-full"></div>
-                <span>Loading...</span>
-            </div>
+            {/* Spinner element with animation */}
+            <div className="animate-spin h-8 w-8 border-4 border-t-transparent border-blue-500 rounded-full"></div>
+            {/* Loading text */}
+            <span>Loading...</span>
+        </div>
     );
 }
